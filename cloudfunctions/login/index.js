9c1198fca314a3c293922950be91c0367e27077a
unknown <unknown@example.com>
2019-04-22 03:34:15 +0800

    login 使用account 和 password 查询数据库

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -8,46 +8,18 @@ cloud.init({
   env: 'studytest1-0726c8'
 })
 
-
-
-// exports.main = (event, context) => {
-//   console.log(event)
-//   console.log(context)
-  const db = cloud.database({
-    env: 'studytest1-0726c8'
-  })
-  const _ = db.command
-//   const ne = db.collection('userinfo').where({
-//     stat: _.eq({
-//       _id: 'XLaaEYnnuWjci1QA'
-//     })
-//   }).get().then(res=>{
-    
-//   })
-//   // 可执行其他自定义逻辑
-//   // console.log 的内容可以在云开发云函数调用日志查看
-
-//   // 获取 WX Context (微信调用上下文)，包括 OPENID、APPID、及 UNIONID（需满足 UNIONID 获取条件）
-//   const wxContext = cloud.getWXContext()
-
-//   return {
-//     ne,
-//     event
-
-//   }
-// }
-
-// exports.main = async (event, context) => await db.collection('userinfo').where({ 
-//       name: '罗才俊'
-//   }).get()
-
+const db = cloud.database({
+  env: 'studytest1-0726c8'
+})
+const _ = db.command
 
 exports.main = async (event, context) =>{
-  const data = await db.collection('userinfo').where({
-    name: '罗才俊'
+  const logindata = await db.collection('userinfo').where({
+    account: event.account,
+    psw:event.psw
   }).get()
-
+  
   return{
-    data
+    data : logindata.data[0]
   }
 } 
