f270f6490a5539ff010e6af21621d4bb3940862e
unknown <unknown@example.com>
2019-04-21 21:14:22 +0800

    login 云函数

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -6,16 +6,20 @@ const cloud = require('wx-server-sdk')
 // 初始化 cloud
 cloud.init()
 
-/**
- * 这个示例将经自动鉴权过的小程序用户 openid 返回给小程序端
- * 
- * event 参数包含小程序端调用传入的 data
- * 
- */
+
+
 exports.main = (event, context) => {
   console.log(event)
   console.log(context)
-
+  const db = cloud.database({
+    env: 'studytest1-0726c8'
+  })
+  const _ = db.command
+  const ne = await db.collection('userinfo').where({
+    stat: _.eq({
+      _id_: 'XLaaEYnnuWjci1QA'
+    })
+  }).get()
   // 可执行其他自定义逻辑
   // console.log 的内容可以在云开发云函数调用日志查看
 
@@ -23,6 +27,8 @@ exports.main = (event, context) => {
   const wxContext = cloud.getWXContext()
 
   return {
+    ne,
     event
+
   }
 }
